6f5b6a39347619a5e02e6251b6b92454eb583ecb
Yovanta Anjelina <yovantaanjelina1001@gmail.com>
2022-05-10 12:02:49 +0700

    feat: add vite app

---
+++ mini-project/src/App.jsx
@@ -0,0 +1,43 @@
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux-store/store';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import HomeLogin from './Pages/HomeLogin'
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import PrivateRoute from './PrivateRoute';
+import Register from './Pages/Register';
+import About from './Pages/About';
+import ContactUs from './Pages/ContactUs';
+import AboutLogin from './Pages/AboutLogin';
+import ContactUsLogin from './Pages/ContactUsLogin';
+import PageNotFound from './Pages/PageNotFound';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Provider store={store}>
+        <PersistGate loading={"loading"} persistor={persistor}>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route element={<PrivateRoute/>}>
+              <Route path="/home-login" element={<HomeLogin/>} />
+            </Route>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/about-login" element={<AboutLogin/>} />
+            <Route path="/contact-us" element={<ContactUs/>} />
+            <Route path="/contact-us-login" element={<ContactUsLogin/>} />
+            <Route path='*' element={<PageNotFound/>}/>
+          </Routes>
+        </PersistGate>
+      </Provider>
+    </div>
+    </Router>
+  )
+}
+
+export default App
